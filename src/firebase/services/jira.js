34bf3337c6dbe4a0d2bacf38b61cb4c96ac6e5cd
luka <lukadiasa@gmail.com>
2024-06-10 19:46:19 +0400

    update jira.js

--- src/firebase/services/jira.js
+++ src/firebase/services/jira.js
@@ -1,44 +1,23 @@
 import axios from 'axios';
 
-const createJiraTicket = async (summary, priority, collectionName, link, userEmail) => {
+const PROXY_SERVER_URL = "https://collectionapp-a928bf60f40f.herokuapp.com/";
+
+export const createJiraTicket = async (summary, priority, collectionName, link, userEmail) => {
   try {
-    const authString = `${process.env.REACT_APP_JIRA_EMAIL}:${process.env.REACT_APP_JIRA_API_TOKEN}`;
-    const base64AuthString = btoa(authString);
+    const response = await axios.post(`${PROXY_SERVER_URL}/create-jira-ticket`, {
+      summary,
+      priority,
+      collectionName,
+      link,
+      userEmail,
+    });
 
-    const response = await axios.post(
-      `https://${process.env.REACT_APP_JIRA_DOMAIN}/rest/api/3/issue`,
-      {
-        fields: {
-          project: {
-            key: process.env.REACT_APP_JIRA_PROJECT_KEY,
-          },
-          summary: summary,
-          description: `Collection: ${collectionName}\nLink: ${link}`,
-          issuetype: {
-            name: 'Task',
-          },
-          priority: {
-            name: priority,
-          },
-          reporter: {
-            emailAddress: userEmail,
-          },
-        },
-      },
-      {
-        headers: {
-          Authorization: `Basic ${base64AuthString}`,
-          'Content-Type': 'application/json',
-        },
-      }
-    );
-
-    console.log('Jira Response:', response.data);
     return response.data;
   } catch (error) {
-    console.error('Error creating Jira ticket:', error.response ? error.response.data : error.message);
+    console.error('Error creating Jira ticket:', error);
     throw new Error('Failed to create Jira ticket');
   }
 };
 
-export { createJiraTicket };
+
+
